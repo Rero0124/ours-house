2793ca2c48b020a70bfcdc1977f12fef6ec43638
rero0124 <kimseung0930@gmail.com>
2023-06-07 01:50:21 +0900

    2023-06-07-01

--- app/api/login/page.tsx
+++ app/api/login/page.tsx
@@ -1,25 +1,25 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { redirect } from 'next/navigation';
 import { open } from 'sqlite';
 import sqlite3 from 'sqlite3';
 
-interface loginRequest extends NextApiRequest {
+interface loginReq extends NextApiRequest{
     body: {
         id: String;
         pw: String;
     };
 }
 
-export default async function CheckUser(req: loginRequest, res: NextApiResponse) {
+export default async function CheckUser(req: loginReq, res: NextApiResponse) {
 
     const db = await open({
         filename: '../user.db',
         driver: sqlite3.Database,
-    }).then();
+    })
 
     console.log(req)
+    console.log(res)
 
     const data = await db.all("select * from user wehre id = '" + req.body.id + "' and pw = '" + req.body.pw + "'")
 
-    res.status(200).json({success: true});
+    res.status(200).json({'succses': true});
 }
--- app/login/section.tsx
+++ app/login/section.tsx
@@ -11,14 +11,15 @@ export default function Section() {
     const userCheck = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const elements = e.currentTarget.elements as loginForm
-        console.log(JSON.stringify({'id': elements.id.value, 'pw': elements.pw.value}))
-        fetch('/api/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({'id': elements.id.value, 'pw': elements.pw.value}),
-        }).then(res => res.json()).then(res => console.log(res))
+        const formData = new FormData();
+        formData.append('id', elements.id.value);
+        formData.append('pw', elements.pw.value);
+        const data = fetch('/api/login', {
+            method: 'post',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            body: formData,
+        }).then(res => res.json()).catch(err => err.message);
+        console.log(data)
     }
 
     return (
